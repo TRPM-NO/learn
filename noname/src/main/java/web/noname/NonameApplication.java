ebc8c3e8fb3f21313e2bab6343c68f50bb8ff766
as <9501hjs@gmail.com>
2022-07-31 03:23:14 +0900

    Init : 초기화
    처음

---
+++ noname/src/main/java/web/noname/NonameApplication.java
@@ -0,0 +1,13 @@
+package web.noname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonameApplication.class, args);
+	}
+
+}
---
+++ noname/src/test/java/web/noname/NonameApplicationTests.java
@@ -0,0 +1,13 @@
+package web.noname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
